0e371a3a64f0a9e48726243242db4fa86e646cdc
adcanada <adam@dapoz.ca>
2025-09-10 10:44:45 -0400

    init, multiline input

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+    cout << "Adam Dapoz 101302979 --- Relational Algebra Processor" << endl;
+    cout << "COMP 3005 Bonus Assignment 1" << endl << endl;
+
+    cout << "Use '|' to signal end of command." << endl;
+    while (true) {
+        cout << "> "; //prompt
+        
+        string query;
+        while (true) { //multi-line input loop
+            string input;
+            getline(cin, input);
+
+            query += input;
+            query += " ";
+
+            if (input.at(input.length()-1) == '|') { break; }
+        }
+
+
+        cout << "got input " << query << endl;
+    }
+
+
+    return 0;
+}
